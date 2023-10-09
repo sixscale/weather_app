0c72d62ebbb15344f085a9b7317120192644a467
Абаев Евгений <abaev@flydrone.ru>
2023-10-09 17:11:29 +0300

    обновил

--- weather/forms.py
+++ weather/forms.py
@@ -7,5 +7,5 @@ class CityForm(ModelForm):
         model = City
         fields = ['name']
         widgets = {
-            'name': TextInput(attrs={'class': 'input', 'placeholder': 'City Name'}),
+            'name': TextInput(attrs={'class': 'input', 'placeholder': 'Название города'}),
         }
