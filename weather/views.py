b351e7a28b5c073e4bb661305bfd594b8b9f5935
Абаев Евгений <abaev@flydrone.ru>
2023-10-09 16:30:53 +0300

    Добавил выбор города в ручную на сайте

--- weather/views.py
+++ weather/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 
 def index(request):
-    url = "http://api.weatherapi.com/v1/current.json?key=596cb3327e754baeb07121429230910&q=Москва&lang=ru"
+    url = "http://api.weatherapi.com/v1/current.json?key=596cb3327e754baeb07121429230910&q={}&lang=ru"
 
-    city = 'Moscow'
-    city_weather = requests.get(url.format(city)).json()
+    cities = City.objects.all()
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+    form = CityForm()
+    weather_data = []
 
-    print(city_weather)
+    for city in cities:
+        city_weather = requests.get(url.format(city)).json()
+        print(city_weather)
 
-    weather = {
-        'city': city_weather['location']['name'],
-        'condition': city_weather['current']['condition']['text'],
-        'temp': round(int(city_weather['current']['temp_c']), 0),
-        #'temp': city_weather['current']['condition']['temp_c']
-    }
+        weather = {
+            'city': city_weather['location']['name'],
+            'country': city_weather['location']['country'],
+            'condition': city_weather['current']['condition']['text'],
+            'temp': round(int(city_weather['current']['temp_c']), 0),
+        }
+        weather_data.append(weather)
 
-    context = {'weather': weather}
+    context = {'weather_data': weather_data, 'form': form}
     return render(request, 'index.html', context)
